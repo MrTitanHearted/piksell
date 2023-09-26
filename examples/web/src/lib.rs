88d5019500bbd3bc03967ea94256823d46ca3b38
Muhammadxon Abduqodirov <abduqodirovmuhammadhon@outlook.com>
2023-09-27 01:09:30 +0500

    web example

--- examples/web/src/lib.rs
+++ examples/web/src/lib.rs
@@ -10,8 +10,11 @@ use winit_input_helper::WinitInputHelper;
 
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub fn run() {
-    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-    console_log::init_with_level(log::Level::Warn).expect("[ERROR]: Couldn't intialize logger!");
+    #[cfg(target_arch = "wasm32")]
+    {
+        std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+        console_log::init_with_level(log::Level::Warn).expect("[ERROR]: Couldn't intialize logger!");
+    }
 
     let mut input = WinitInputHelper::new();
 
